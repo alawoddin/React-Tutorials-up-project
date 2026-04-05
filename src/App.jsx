8af68a25afdc60a77e73708d7a2acd2fafc9e270
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-04-05 21:50:59 +0430

    the react all or full tutrial is start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    {/* <h1>hello world</h1> */}
+
+    {/* <h1>i am alawoddin {20 + 20}</h1> */}
+
+    <h1>the value is {2 * 3}</h1>
+    </>
+  )
+}
+
+export default App

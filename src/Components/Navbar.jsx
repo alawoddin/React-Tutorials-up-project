6d9340eeccdf36c19e13d7857545c9b2f8e8553c
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-04-07 11:25:23 +0430

    destructuring is done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Navbar = (props) => {
+const Navbar = ({ title , age }) => {
     return (
         <div>
             <h1>Navbar</h1>
-            <p>the title is {props.title}</p>
-            <p>the age is {props.age}</p>
-            <p>the job is {props.job}</p>
+            <p>the title is {title}</p>
+            <p>the age is {age}</p>
+            {/* <p>the job is {props.job}</p> */}
         </div>
     );
 };

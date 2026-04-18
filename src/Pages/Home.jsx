11430d9fa15c1fc0ef528cc9f8106ee4cab15015
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-04-18 22:59:34 +0430

    the page is also ad

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Nav from '../Components/Nav';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import Modal from '../Components/Modal';
+import Banner from './Banner';
+import Categories from './Categories';
+import Featured from './Featured';
 
 const Home = () => {
     return (
@@ -9,6 +13,14 @@ const Home = () => {
 
             <Nav />
             <Header />
+
+            <Modal />
+
+            <Banner />
+
+            <Categories />
+
+            <Featured />
 
             <Footer />
             

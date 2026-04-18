1ebe4888fdc335ea7d9a4be3778cabe81ad768bc
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-04-18 22:36:58 +0430

    i remove all the file

--- src/App.jsx
+++ src/App.jsx
@@ -1,132 +1,18 @@
 
 // import { useState } from 'react';
 import './App.css';
-// import styles from './style.module.css'
-import Demo from './App/Demo'
-import List from './Components/List'
-import Form from './Components/Event/Form';
-import Textarea from './Components/Event/Textarea';
-import Task from './Components/Task/Task';
-import Opation from './Components/Event/Opation';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import Main from './Components/State/Main';
-import Effect from './Components/State/Effect';
-import Content from './Components/State/Content';
-import UseRef from './Components/State/UseRef';
+
 
 
 function App() {
 
-  // const name = "alawoddin";
-
-  // const active = false;
-
-  // const x = 10;
-  
-  // const isLogin = false;
-
-  // const [name , setName] = useState("");
-
-  // const [fname , setFname] = useState("");
-
-  function Home() {
-  return <h1>Home Page</h1>;
-}
-
-function About() {
-  return <h1>About Page</h1>;
-}
-
-function Contact() {
-  return <h1>Contact Page</h1>;
-}
 
 
 
   return (
     <>
 
-    {/* <Main /> */}
-    {/* <Effect /> */}
-
-    {/* <Content /> */}
-    <UseRef />
-
-    {/* <useReducer /> */}
-    {/* <BrowserRouter>
-    <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/contact'>Contact</Link>
-    </nav>
-
-
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/contact' element={<Contact/>} />
-    </Routes>
-    </BrowserRouter> */}
-
-    {/* <List /> */}
-
-    {/* <Form /> */}
-
-    {/* <Textarea /> */}
-
-    {/* <Task /> */}
-
-    {/* <h1 className={styles.h1}>hello world</h1> */}
-
-    {/* <Opation /> */}
-
-
-
-{/* 
-    <input type="text" onChange={(e) => setName(e.target.value) } name="" id="" />
-
-    {name && <h1>your name is : {name}</h1>}
-
-
-    <input type="text" onClick={(e) => setFname(e.target.value)} />
-
-     <h1>your fname is {fname}</h1>
-
-
-    
-    {isLogin ? (
-      <h1>welcome to the class</h1>
-    ) : (
-      <h1>i am sorry your login give error </h1>
-    )} */}
-
-      {/* {isLogin ? (
-        <h1>welcome to the class</h1>
-      ) : (
-        <h1> i am sorry you can not login</h1>
-      )} */}
-
-    {/* <Demo /> */}
-
-
-    {/* <h1>hello world</h1> */}
-
-    {/* <h1>i am alawoddin {20 + 20}</h1> */}
-
-    {/* <h1>the value is {2 * 3}</h1> */}
-
-    {/* <h1 className='myclass' style={{color: "blue"}}>
-      hello {20 + 20} {name}
-    </h1>
-
-    <button disabled={active}>click me</button>
-
-
-    {x === 10 ? <h1> the value is 10 </h1> : <h1>the value is not 10</h1>}
-
-    { x === 10 && <h1>the value is 10</h1>}
-
-    {x === 10 && <h1> the value is 10 </h1>} */}
+    <h1>hello world</h1>
 
 
 
